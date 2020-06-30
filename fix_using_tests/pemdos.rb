241a6ee0e5158195c0356d447ab04255086934e7
Daniel Cole <danielscole1@gmail.com>
2020-06-30 21:31:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,8 +4,16 @@ require 'pry'
 def snake_it_up(string)
   snake_talk = "s"
   if string[0] == "s"
+<<<<<<< HEAD
     snake_talk * 10 + string
     else
     string
     end
+=======
+    10 * "s" + string
+    else
+    string
+    binding.pry
+  end
+>>>>>>> 4fa6b5d7c341961f4a172163324c13c3da59bf2a
 end
